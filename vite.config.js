84d525c62e89de8b7355cd9e3109308d5f8a22bd
marcic98 <marcic.aca@gmail.com>
2026-09-21 17:39:02 +0200

    Fix Bolt hosting build output configuration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { host: true }
+  base: '/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  server: { host: true },
 })
